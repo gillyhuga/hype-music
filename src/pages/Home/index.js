d55366a8afa519796e99a80288fe16a3242b26ff
Gilly <gilly.huga@yahoo.co.id>
2022-03-25 23:01:28 +0700

    chore: change to tailwind css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import CardTrack from "../../components/CardTrack";
+import Card from "../../components/Card";
 import data from "../../data/single-sample";
 
 
 function Home() {
     return (
         <div >
-            <CardTrack
+            <Card
                 title={data.name}
                 artists={data.artists[0].name}
                 images={data.album.images[0].url}
